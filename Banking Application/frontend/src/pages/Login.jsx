c4bc45574114797e79c355a4bb2cf5190d0525dd
Sourav Bera <souravbera6353@gmail.com>
2025-11-13 23:38:25 +0530

    bank done

---
+++ Banking Application/frontend/src/pages/Login.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from '../context/authContext'
+
+const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState(null)
+    const navigate = useNavigate()
+    const {login} = useAuth()
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await axios.post(
+                "http://localhost:5000/api/auth/login", 
+                {email, password}
+            );
+            if(response.data.success) {
+                login(response.data.user)
+                console.log(response.data.user)
+                localStorage.setItem("token", response.data.token)
+                if(response.data.user.role === "admin") {
+                    navigate('/admin-dashboard')
+                } else {
+                    navigate('/employee-dashboard')
+                }
+           }
+        } catch(error) {
+            if(error.response && !error.response.data.success) {
+                setError(error.response.data.error);
+            } else {
+                setError("server error")
+            }
+            console.log(error)
+        }
+    }
+
+  return (
+    <div className='flex flex-col items-center h-screen justify-center 
+    bg-gradient-to-b from-teal-600 from-50% to-gray-100 to-50% space-y-6'>
+        <h2 className='google-font text-3xl text-white'>Employee Management System</h2>
+        <div className='border shadow p-6 w-80 bg-white'>
+        <h2 className='text-2xl font-bold mb-4'>Login</h2>
+        {error && <p className='text-red-500'>{error}</p>}
+        <form onSubmit={handleSubmit}>
+            <div className='mb-4'>
+                <label htmlFor='email' className='block text-gray-700'>Email</label>
+                <input 
+                type='email' 
+                className='w-full px-3 py-2 border'
+                placeholder='Enter Email'
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                />
+                
+            </div>
+            <div>
+                <label htmlFor='password' className='block text-gray-700'>Password</label>
+                <input 
+                type='password' 
+                className='w-full px-3 py-2 border'
+                placeholder='############'
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                />
+            </div>
+            <div className='mb-4 mt-4'>
+                <button
+                type='submit'
+                className='w-full bg-teal-600 text-white py-2'
+                >
+                Login
+                </button>
+            </div>
+        </form>
+        </div>
+    </div>
+  )
+}
+
+
+export default Login
